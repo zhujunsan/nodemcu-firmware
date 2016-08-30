55cdc3debf2cb719628025d39983595e414ab19a
zhujunsan <github@zhujunsan.net>
2016-08-30 13:37:43 +0800

    add chinese doc 2

--- docs/js/extra.js
+++ docs/js/extra.js
@@ -1,8 +1,8 @@
 var nodemcu = nodemcu || {};
 (function () {
   'use strict';
-  //var languageCodeToNameMap = {en: 'English', de: 'Deutsch'};
-  var languageCodeToNameMap = {en: 'English'};
+  var languageCodeToNameMap = {en: 'English', de: 'Deutsch', zh_CN: 'Simplified Chinese'};
+  //var languageCodeToNameMap = {en: 'English'};
   var languageNames = values(languageCodeToNameMap);
   var defaultLanguageCode = 'en';
 
